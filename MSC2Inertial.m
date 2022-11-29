c562a84218c9b8391332c86b050092cba452cdc8
matthew-givens <39773260+matthew-givens@users.noreply.github.com>
2022-11-29 16:57:08 -0700

    Add files via upload

---
+++ MSC2Inertial.m
@@ -0,0 +1,13 @@
+function x = MSC2Inertial(y)
+
+u4 = y(5)*cos(y(1))*cos(y(3)) - y(2)*sin(y(1))*cos(y(3)) - y(4)*sin(y(3));
+u5 = y(5)*cos(y(1))*sin(y(3)) - y(2)*sin(y(1))*sin(y(3)) + y(4)*cos(y(3));
+
+x = 1/(y(6))*[cos(y(1))*cos(y(3));
+              cos(y(1))*sin(y(3));
+              sin(y(1));
+              u4;
+              u5;
+              y(5)*sin(y(1))+y(2)*cos(y(1))];
+
+end
